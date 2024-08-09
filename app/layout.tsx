37db05e04ce319789c206dda27ae9d834aa91838
priyasha2011 <priyashamohanty03@gmail.com>
2024-08-09 18:27:37 +0530

    text to voice added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { cn } from "../lib/utils"; // Import the cn function
+import { Menu, MenuItem, ProductItem, HoveredLink } from "../components/navbar/page"; // Import the Navbar component
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className)}>
+
+        {children}
+      </body>
     </html>
   );
 }
